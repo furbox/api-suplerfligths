bcc0c753b8af693e6581f6e12721d0150e277c0e
furbox <furbox@gmail.com>
2021-08-15 18:47:04 -0500

    add user

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { AppModule } from './app.module';
+import { AllExceptionFilter } from './common/filters/http-exceptions.filter';
+import { TimeOutInterceptor } from './common/interceptors/timeout.interceptor';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({
+      logger: true,
+    }),
+  );
+  app.useGlobalFilters(new AllExceptionFilter());
+  app.useGlobalInterceptors(new TimeOutInterceptor());
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(3000);
+}
+bootstrap();
